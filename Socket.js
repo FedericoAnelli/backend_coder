1179652fa6394d6d74b014a415f08eb2476cc611
Federico Anelli <federicoanelli@gmail.com>
2022-10-05 14:35:38 -0300

    Chat funcionando

--- Socket.js
+++ Socket.js
@@ -1,6 +1,7 @@
 const { Server } = require('socket.io');
 
 let io;
+let messages = [];
 
 class Socket{
     static init(httpServer){
@@ -9,12 +10,16 @@ class Socket{
         io.on('connection', (socket) => {
             console.log('a user connected', socket.id);
 
-            socket.on('message', (msg) => {
-                io.emit('message', {id: socket.id, message: msg});
+            socket.on('titleChange', (msg) => {
+                io.emit('titleChange', {id: socket.id, message: msg});
             });
 
             socket.on('priceChange', (msg) => {
                 io.emit('priceChange', msg);
+            });
+
+            socket.on('message', (userEmail, message) => {
+                io.emit('message', {email: userEmail, message: message});
             });
 
             socket.on('disconnect', () => {
--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -3,27 +3,41 @@
     const productPrice = document.getElementById('price');
     const newItemTitle = document.getElementById('newItemTitle');
     const newItemPrice = document.getElementById('newItemPrice');
+    const userEmail = document.getElementById('email');
+    const userMessage = document.getElementById('message');
+    const sendMessage = document.getElementById('sendMessage');
+    const messagesContainer = document.getElementById('messagesContainer');
 
     const socket = io();
 
     productTitle.addEventListener('keyup', function(event){
-        socket.emit('message', event.target.value);
+        socket.emit('titleChange', event.target.value);
     });
 
     productPrice.addEventListener('keyup', function(event){
         socket.emit('priceChange', event.target.value);
     });
 
+    sendMessage.addEventListener('click', function(event){
+        event.preventDefault();
+        socket.emit('message', userEmail.value, userMessage.value);
+    });
+
     socket.on('connect', () => {
         console.log('Connected');
     });
 
-    socket.on('message', (msg) => {
+    socket.on('titleChange', (msg) => {
         newItemTitle.innerText = msg.message;
     })
 
     socket.on('priceChange', (msg) => {
         newItemPrice.innerText = msg;
     })
+
+    socket.on('message', (msg) => {
+        messagesContainer.innerHTML += `<div class="alert alert-info" role="alert">${msg.email}: ${msg.message}</div>`
+    }
+    )
 
 })();
