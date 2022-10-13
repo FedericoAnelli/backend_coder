31463bdeab77861a0a4f2dd48b5bec600dfd0749
Federico Anelli <federicoanelli@gmail.com>
2022-10-12 23:31:36 -0300

    minor fixes

--- routes/carrito.js
+++ routes/carrito.js
@@ -1,42 +1,80 @@
 var express = require('express');
 var router = express.Router();
 
-const productos = [];
+const date = new Date();
+const year = date.getFullYear();
+const month = date.getMonth();
+const day = date.getDate();
+const hour = date.getHours();
+const minutes = date.getMinutes();
+const seconds = date.getSeconds();
+
+
+const carrito = [
+    {
+        id: 1,
+        timestamp: `${year}-${month}-${day} ${hour}:${minutes}:${seconds}`,
+        productos: [
+            {
+                id: 1,
+                title: 'Escuadra',
+                timestamp: `${year}-${month}-${day} ${hour}:${minutes}:${seconds}`,
+                description: 'Escuadra de 30 cm',
+                code: '123456',
+                price: 123.45,
+                stock: 10,
+                thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png'
+            },
+            {
+                id: 2,
+                title: 'Calculadora',
+                timestamp: `${year}-${month}-${day} ${hour}:${minutes}:${seconds}`,
+                description: 'Calculadora científica',
+                code: '234567',
+                price: 234.56,
+                stock: 12,
+                thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/calculator-math-tool-school-256.png'
+            },
+        ]
+    },
+];
 
 let id = 1;
+let selectedProducts = [];
 
 router.post('/' , (req, res) => {
     const producto = req.body;
     producto.id = id++;
-    productos.push(producto);
-    res.render("productos", {productos});
+    carrito.push(producto);
+    res.render("carrito", {carrito});
 });
 
 router.delete('/:id', (req, res) => {
-    const producto = productos.find(p => p.id == req.params.id);
-    const index = productos.indexOf(producto);
-    productos.splice(index, 1);
-    res.render("productos", {productos});
+    const producto = carrito.find(p => p.id == req.params.id);
+    const index = carrito.indexOf(producto);
+    carrito.splice(index, 1);
+    res.render("carrito", {carrito});
 });
 
-router.get('/:id/productos' , (req, res) => {
-    const producto = productos.find(p => p.id == req.params.id);
-    res.render("productos", {producto});
+router.get('/:id/carrito' , (req, res) => {
+    const producto = carrito.find(p => p.id == req.params.id);
+    selectedProducts = [producto];
+    res.render("carrito", {selectedProducts});
 });
 
-router.post('/:id/productos' , (req, res) => {
-    const producto = productos.find(p => p.id == req.params.id);
+router.post('/:id/carrito' , (req, res) => {
+    const producto = carrito.find(p => p.id == req.params.id);
     producto.title = req.body.title;
     producto.price = req.body.price;
     producto.thumbnail = req.body.thumbnail;
-    res.render("productos", {productos});
+    res.render("carrito", {carrito});
 });
 
-router.delete('/:id/productos/:id_producto' , (req, res) => {
-    const producto = productos.find(p => p.id == req.params.id);
-    const index = productos.indexOf(producto);
-    productos.splice(index, 1);
-    res.render("productos", {productos});
+router.delete('/:id/carrito/:id_producto' , (req, res) => {
+    const producto = carrito.find(p => p.id == req.params.id);
+    const index = carrito.indexOf(producto);
+    carrito.splice(index, 1);
+    res.render("carrito", {carrito});
 });
 
 
--- routes/productos.js
+++ routes/productos.js
@@ -1,37 +1,61 @@
 var express = require('express');
 var router = express.Router();
 
+const date = new Date();
+const year = date.getFullYear();
+const month = date.getMonth();
+const day = date.getDate();
+const hour = date.getHours();
+const minutes = date.getMinutes();
+const seconds = date.getSeconds();
+
 const productos = [
 
     {
         id: 1,
+        timestamp: `${year}-${month}-${day} ${hour}:${minutes}:${seconds}`,
         title: 'Escuadra',
+        description: 'Escuadra de 30 cm',
+        code: '123456',
         price: 123.45,
+        stock: 10,
         thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png'
     },
     {
         id: 2,
+        timestamp: `${year}-${month}-${day} ${hour}:${minutes}:${seconds}`,
         title: 'Calculadora',
+        description: 'Calculadora científica',
+        code: '234567',
         price: 234.56,
+        stock: 12,
         thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/calculator-math-tool-school-256.png'
     },
     {
         id: 3,
+        timestamp: `${year}-${month}-${day} ${hour}:${minutes}:${seconds}`,
         title: 'Globo Terráqueo',
+        description: 'Globo terráqueo de 30 cm',
+        code: '345678',
         price: 345.67,
+        stock: 10,
         thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/globe-earth-geograhy-planet-school-256.png'
     }
 ];
 
 let id = 4;
+let selectedProducts = [];
 
 router.get('/', (__, res) => {
-    res.render("productos", {productos});
+    selectedProducts = productos;
+    res.render("productos", {selectedProducts});
 });
 
 router.get('/:id', (req, res) => {
     const producto = productos.find(p => p.id == req.params.id);
-    res.render("productos", {producto});
+    selectedProducts = [producto];
+    console.log(selectedProducts);
+    res.render("productos", {selectedProducts});
     }
 );
 
